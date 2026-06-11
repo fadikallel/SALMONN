a445c829ca20cf4f6a99cd3971f16a83298919b2
fedi kallel <fedi.kallel@gmail.com>
2026-06-11 10:49:38 +0200

    for wav2vec evaluation

--- cli_inference.py
+++ cli_inference.py
@@ -67,7 +67,7 @@ args.cfg_path = "configs/qwen_config_dfd.yaml"
 cfg = Config(args)
 run_config = cfg.config.run
 model_config = cfg.config.model
-model_config.ckpt = "outputs/asv19_no_reasoning/checkpoint_best.pth" 
+model_config.ckpt = "outputs/wav2vec_qwen_asv19_no_reasoning/checkpoint_best.pth" 
 data_config = cfg.config.datasets
 init_distributed_mode(run_config)
 
