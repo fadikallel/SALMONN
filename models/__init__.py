ed6c92a7b7c57a8642441bcfe583be911cb6b37f
fedi kallel <fedi.kallel@gmail.com>
2026-06-22 18:31:26 +0200

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -15,7 +15,7 @@
 # from .allm import ALLM 
 # from .qwen_audio import ALLM
 from .qwen_audio_wav2vec import ALLM
-from .salmonn import SALMONN
+# from .salmonn import SALMONN
 
 def load_model(config):
-    return SALMONN.from_config(config)
+    return ALLM.from_config(config)
